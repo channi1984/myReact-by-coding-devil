3de79c4fc07a45b3db90280fb1cd44b79b7a9e25
channi <channi.kr@gmail.com>
2021-03-23 21:47:43 +0900

    bbb

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom';
-import dummy from '../db/data.json';
+import { useEffect, useState } from 'react';
 
 export default function DayList() {
-    return <ul className="list_day">
-        {dummy.days.map(day => (
-            <li key={day.id}>
-                <Link to={`/day/${day.day}`}>
-                    Day {day.day}
-                </Link>
-            </li>
-        ))}
-        <li></li>
-    </ul>
+    const [days, setDays] = useState([]);
+
+    useEffect(() => {
+        console.log("Count change");
+    }, []);
+
+    return (
+        <ul className="list_day">
+            {days.map(day => (
+                <li key={day.id}>
+                    <Link to={`/day/${day.day}`}>
+                        Day {day.day}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
 }
